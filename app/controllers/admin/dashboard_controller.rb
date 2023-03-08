1b6f1bf3f31f82334765c6ea4195a1d31e2ed048
JasonSnow123 <m.jason.2018@gmail.com>
2023-03-08 18:16:13 -0500

    Add dashboard content to display information

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASSWORD"]
   
   def show
+    @products = Product.count("id")
+    @categories = Product.select("DISTINCT category_id")
+    @number_of_orders = LineItem.select("DISTINCT order_id")
   end
 end
