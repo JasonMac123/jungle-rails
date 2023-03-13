03ca381f3f716b4b3b318a5f61577a65205ebb02
JasonSnow123 <m.jason.2018@gmail.com>
2023-03-13 18:22:30 -0400

    Add user tests and add user authentication functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-  
   has_secure_password
-
+  validates :email, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  
+  def self.authenticate_with_credentials(email, password)
+      no_space_email = email.strip
+      user = User.where("email ILIKE '#{no_space_email}'").first
+      
+      if user && user.authenticate(password)
+        return user
+      end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,76 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations for users' do
+    it 'user is created if all details are valid' do
+      @user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'password')
+      expect(@user.save).to eq(true)
+    end
+
+    it 'user is not created if email is not unique' do
+      @user1 = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'password')
+      @user2 = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'password')
+      expect(@user2.save).to eq(false)
+    end
+    
+    it 'user is not created when passwords do not match' do
+      @user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'passwords')
+      expect(@user.save).to eq(false)
+    end
+
+    it 'user is not created when first name is not given' do
+      @user = User.create(last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'passwords')
+      expect(@user.save).to eq(false)
+    end
+
+    it 'user is not created when last name is not given' do
+      @user = User.create(first_name: 'Jason', email: 'testing@gmail.com', password: 'password', password_confirmation: 'passwords')
+      expect(@user.save).to eq(false)
+    end
+
+    it 'user is not created when email is not given' do
+      @user = User.create(first_name: 'Jason', last_name: 'Mac', password: 'password', password_confirmation: 'passwords')
+      expect(@user.save).to eq(false)
+    end
+
+    it 'user is not created when password is not given' do
+      @user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password_confirmation: 'passwords')
+      expect(@user.save).to eq(false)
+    end
+
+    it 'user is not created when confirmation password is not given' do
+      @user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password')
+      expect(@user.save).to eq(false)
+    end    
+
+    it 'user is not created if password meet minimum length' do
+      @user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'p', password_confirmation: 'p')
+      expect(@user.save).to eq(false)
+    end
+  end 
+
+  describe '.authenticate_with_credentials' do
+    it 'should return user if email and user name are correct' do
+      @new_user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'password')
+      @user = User.authenticate_with_credentials('testing@gmail.com', 'password')
+      expect(@user).not_to be_nil
+    end
+  
+    it 'should return nil if password is wrong' do
+      @new_user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'password')
+      @user = User.authenticate_with_credentials('testing@gmail.com', '123123123')
+      expect(@user).to be_nil
+    end
+
+    it 'should return user if there are spaces in the beginner and at the end' do
+      @new_user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'password')
+      @user = User.authenticate_with_credentials(' testing@gmail.com ', 'password')
+      expect(@user).not_to be_nil
+    end
+
+    it 'should return user as email is case insensitive' do
+      @new_user = User.create(first_name: 'Jason', last_name: 'Mac', email: 'testing@gmail.com', password: 'password', password_confirmation: 'password')
+      @user = User.authenticate_with_credentials(' TESTING@gmail.com ', 'password')
+      expect(@user).not_to be_nil
+    end
   end
 end
